24fe2090b5848629a092e4436f44a3c0a69759e6
aniketkalvit <aniketkalvit@gmail.com>
2022-09-06 10:26:51 +0530

    Add project files.

---
+++ FirstMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using FirstMvcDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is sample mvc application";
+            List<string> colorList = new List<string>() {
+                    "Red","Green","Blue","Yellow","Black","White","Purple","Pink"
+            };
+           
+            ViewData["list"] = colorList;
+
+            ViewBag.ColorList=colorList;
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
